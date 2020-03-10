bf7979a8045d5e7675d2f2175d3a05b40513add4
sravyau <62027295+sravyau@users.noreply.github.com>
2020-03-10 16:16:51 -0400

    cs680 homework1

---
+++ Homework1/src/edu/umb/cs680/hw01/Calculator.java
@@ -0,0 +1,21 @@
+package edu.umb.cs680.hw01;
+
+public class Calculator {
+
+	public float multiply(float a, float b){
+		return a*b;
+	}
+
+	public float divide (float a, float b) {
+		if(b==0){
+			throw new IllegalArgumentException("division by zero");
+		}
+		return a/b;
+	}
+	
+	public static void main(String[] args){
+		Calculator	calc = new Calculator();
+		System.out.println( calc.multiply(6, 9) );
+		System.out.println( calc.divide(125, 5) );
+	}	
+}
---
+++ Homework1/src/edu/umb/cs680/hw01/CalculatorTest.java
@@ -0,0 +1,52 @@
+package edu.umb.cs680.hw01;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	public void multiply6By2() {
+		Calculator cut = new Calculator();
+		float expected = 12;
+		float actual = cut.multiply(6, 2);
+		assertTrue(cut instanceof Calculator);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void divide2_5By5_5() {
+		Calculator cut = new Calculator();
+		float expected = (float) 2;
+		float actual = cut.divide(10, 5);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void divide3By2() {
+		Calculator cut = new Calculator();
+		float expected = (float) 1.5;
+		float actual = cut.divide(3, 2);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void divide5By0withTryCatch() {
+		Calculator cut = new Calculator();
+		try {
+			cut.divide(10, 0);
+			fail("Division by zero");
+		} catch (IllegalArgumentException ex) {
+			assertEquals("division by zero", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void multiple2_5By5_5() {
+		Calculator cut = new Calculator();
+		float expected = (float) 25;
+		float actual = cut.multiply(5, 5);
+		assertEquals(expected, actual);
+	}
+
+}
