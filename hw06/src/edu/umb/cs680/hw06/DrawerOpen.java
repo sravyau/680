a93d8f09f00058344959032ed32b379a7b33cc15
sravyau <62027295+sravyau@users.noreply.github.com>
2020-03-31 07:32:50 +0530

    Add files via upload

---
+++ hw06/src/edu/umb/cs680/hw06/DrawerOpen.java
@@ -0,0 +1,29 @@
+package edu.umb.cs680.hw06;
+
+public class DrawerOpen implements State {
+	private DrawerOpen() {
+	}
+
+	private static DrawerOpen instance = null;
+
+	public static DrawerOpen getInstance() {
+		if (instance == null)
+			instance = new DrawerOpen();
+		return instance;
+	}
+
+	public void openCloseButtonPushed() {
+		dvdPlayer.close();
+		dvdPlayer.changeState(DrawerClosedNotPlaying.getInstance());
+	}
+
+	public void playButtonPushed() {
+		dvdPlayer.close();
+		dvdPlayer.changeState(DrawerClosedPlaying.getInstance());
+	}
+
+	public void stopButtonPushed() {
+		System.out.println("Stop Playing DVD....");
+	}
+
+}
